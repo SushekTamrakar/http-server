c805a22dc2b6bdf858535019ce8e006cfda1902d
sushektamrakar <sushek_3@hotmail.com>
2023-12-03 18:16:25 +0545

    feat: connection establish between client and server

--- client.py
+++ client.py
@@ -1,12 +1,34 @@
 import socket
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect('127.0.0.1', 9000)
-cmd = 'GET http://127.0.0.0.1/test.txt hTTP/1,0\r\n'.encode()
-mysock.send(cmd)
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE=  "******** CONNECTION LOST *******************"
+SERVER = '127.0.1.1'
+ADDR = (SERVER, PORT)
 
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode(), end='')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    message_length = len(message)
+    send_length = str(message_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("FIRST MESSAGE")
+send(DISCONNECT_MESSAGE)
+
+
+# cmd = 'GET http://127.0.0.0.1/test.txt hTTP/1,0\r\n'.encode()
+# mysock.send(cmd)
+
+# while True:
+#     data = mysock.recv(512)
+#     if len(data) < 1:
+#         break
+#     print(data.decode(), end='')
--- server.py
+++ server.py
@@ -1,34 +1,88 @@
-from socket import *
-
-def createServer():
-    serversocket = socket(AF_INET, SOCK_STREAM)
-
-    try:
-        serversocket.bind(('localhost', '9000'))
-        serversocket.listen(5)
-
-        while(1):
-            (clientsocket, address) = serversocket.accept()
-            rd = clientsocket.recv(5000).decode()
-            pieces = rd.split("\n")
-            if ( len(pieces)> 0) : print(pieces[0])
-
-            data = "HTTP/1.1 200 OK \r\n"
-            data += "Content-Type: text/html; charset=utf-8\r\n"
-            data += '\r\n'
-            data += "<html><body>Hello World</body></html>\r\n\r\n"
-            clientsocket.sendall(data.encode())
-            clientsocket.shutdown(SHUT_WR)
+import socket
+import threading
 
 
-    except KeyboardInterrupt :
-        print("\nShutting Down...\n")
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE=  "******** CONNECTION LOST *******************"
 
-    except Exception as exc :
-        print("Error")
-        print(exec)
+ADDR = (SERVER, PORT)
 
-    serversocket.close()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # For IPV6 addresses AF_INET
 
-print('Access https://localhost:9000')
-createServer() 
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print("New connection----------------")
+    print(addr, ' - conected')
+
+    conncted = True
+
+    while conncted:
+        message_length = conn.recv(HEADER).decode(FORMAT)
+        if message_length:
+            message_length = int(message_length)
+            msg = conn.recv(message_length).decode(FORMAT)
+            print(addr, '-', msg)
+            if msg == DISCONNECT_MESSAGE:
+                conncted = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("message recieved".encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print("Server is Listening on ", SERVER)
+    while True:
+        conn, addr = server.accept() #conn is connection that we ginna send the information back and addr is the information about the cinnection
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        active_connection = threading.active_count() - 1
+        print("Active Connection ", active_connection)
+
+
+print("Server is starting.....................")
+start()
+
+
+
+
+
+
+# def createServer():
+#     serversocket = socket(AF_INET, SOCK_STREAM)
+
+#     try:
+#         serversocket.bind(('localhost', '9000'))
+#         serversocket.listen(5)
+
+#         while(1):
+#             (clientsocket, address) = serversocket.accept()
+#             rd = clientsocket.recv(5000).decode()
+#             pieces = rd.split("\n")
+#             if ( len(pieces)> 0) : print(pieces[0])
+
+#             data = "HTTP/1.1 200 OK \r\n"
+#             data += "Content-Type: text/html; charset=utf-8\r\n"
+#             data += '\r\n'
+#             data += "<html><body>Hello World</body></html>\r\n\r\n"
+#             clientsocket.sendall(data.encode())
+#             clientsocket.shutdown(SHUT_WR)
+
+
+#     except KeyboardInterrupt :
+#         print("\nShutting Down...\n")
+
+#     except Exception as exc :
+#         print("Error")
+#         print(exec)
+
+#     serversocket.close()
+
+# print('Access https://localhost:9000')
+# createServer() 

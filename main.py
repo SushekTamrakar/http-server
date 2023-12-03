44b4e6460ede465ac0217ad9efa63cc6d725248e
sushektamrakar <sushek_3@hotmail.com>
2023-12-03 14:38:31 +0545

    feat: add main.py file

---
+++ main.py
@@ -0,0 +1,39 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from datetime import time
+
+HOST = "172.19.29.72"
+PORT = 9999
+
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+
+        self.end_headers()
+
+        self.wfile.write(bytes("<html><body><h1> HELLO WORLD! </body></html>", "utf-8"))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+
+        self.end_headers()
+
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+        self.wfile.write(bytes(date))
+
+
+server = HTTPServer((HOST, PORT), HTTPRequestHandler)
+print("Server is now running in Port", HOST)
+
+
+server.serve_forever()
+server.server_close()
+print('Server Stopped')
+
+
+
+
+    
